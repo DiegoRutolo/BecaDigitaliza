93da070ff556a989be65224843bdecf5b5d49f84
DiegoRutolo <diegorutolo@gmail.com>
2019-02-14 12:30:52 +0100

    Ejercicio 7 (for)

---
+++ 06_for.py
@@ -0,0 +1,7 @@
+nombres = ["Alba", "Brais", "Cholo", "Dora", "Eusebio", "Francisco", "Gloria", "Honorino"]
+selected = []
+for n in nombres:
+    print(n)
+    if 'a' in n:
+        selected.append(n)
+print(selected)
